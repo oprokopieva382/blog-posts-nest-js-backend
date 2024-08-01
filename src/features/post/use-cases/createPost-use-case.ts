4cbf148c9243248ce6bd618bca823869100605f0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-08-01 13:41:43 -0500

    create cqrs use case and command for create post

---
+++ src/features/post/use-cases/createPost-use-case.ts
@@ -0,0 +1,20 @@
+import { ICommandHandler } from '@nestjs/cqrs';
+import { PostRepository } from '../post.repository';
+import { PostInputModel } from '../DTOs/input/PostInputModel.dto';
+
+export class CreatePostCommand {
+  constructor(public dto: PostInputModel) {}
+}
+
+export class CreatePostUseCase implements ICommandHandler<CreatePostCommand> {
+  constructor(private readonly postRepository: PostRepository) {}
+
+  async execute(command: CreatePostCommand) {
+    const postDto = {
+      ...command.dto,
+      blog: command.dto.blogId,
+      createdAt: new Date(),
+    };
+    return await this.postRepository.createPost(postDto);
+  }
+}
