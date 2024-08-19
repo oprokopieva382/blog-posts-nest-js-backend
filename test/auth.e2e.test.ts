2f489f9e87d6510e55bbaf8e131b7145977a87dd
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-08-19 11:54:07 -0500

    create login user e2e tests

--- test/auth.e2e.test.ts
+++ test/auth.e2e.test.ts
@@ -26,39 +26,61 @@ describe('auth tests', () => {
     await request(app.getHttpServer()).delete('/testing/all-data').expect(204);
   });
 
-//   describe('1. (GET) - GET COMMENT BY ID', () => {
-//     it('1. Should get comment by id & return status code 200', async () => {
-//       //create user
-//       const newUser = {
-//         login: 'Tina',
-//         password: 'tina123',
-//         email: 'Tina@gmail.com',
-//       };
+  describe('1. (POST) - AUTH LOGIN', () => {
+    it('1. Should login user and return status code 200', async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
 
-//       await request(app.getHttpServer())
-//         .post('/users')
-//         .send(newUser)
-//         .auth('admin', 'qwerty')
-//         .expect(201);
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
 
-//       //login user
-//       const loginInput = {
-//         loginOrEmail: 'Tina',
-//         password: 'tina123',
-//       };
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123',
+      };
 
-//       const res = await request(app.getHttpServer())
-//         .post('/auth/login')
-//         .send(loginInput)
-//         .expect(200);
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(200);
 
-//       //set accessToken
-//       const accessToken = res.body.accessToken;
+      expect(res.body).toEqual({
+        accessToken: expect.any(String),
+      });
+    });
 
-      
-//     });
+    it("2. Shouldn't login user and return status code 401 if password or login is wrong", async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
 
-  
-   
-//   });
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
+
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123456789',
+      };
+
+      await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(401);
+    });
+  });
 });
