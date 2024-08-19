06f0de7d5a96a1687814af4a5bd603db75831d13
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-08-19 13:10:40 -0500

    create e2e tests for auth/me and auth/logout

--- test/auth.e2e.test.ts
+++ test/auth.e2e.test.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as cookieParser from 'cookie-parser';
 import { INestApplication } from '@nestjs/common';
 import { applyAppSettings } from '../src/settings/apply-app-settings';
 import * as request from 'supertest';
@@ -14,6 +15,8 @@ describe('auth tests', () => {
 
     app = moduleFixture.createNestApplication();
     applyAppSettings(app);
+
+    app.use(cookieParser());
 
     await app.init();
   });
@@ -80,6 +83,146 @@ describe('auth tests', () => {
       await request(app.getHttpServer())
         .post('/auth/login')
         .send(loginInput)
+        .expect(401);
+    });
+  });
+  describe('2. (GET) - AUTH ME', () => {
+    it('1. Should auth me return status code 200 and object', async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
+
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
+
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123',
+      };
+
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(200);
+
+      //get accessToken
+      const accessToken = res.body.accessToken;
+      await request(app.getHttpServer())
+        .get('/auth/me')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200);
+    });
+
+    it("2. Shouldn't auth me and return status code 401", async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
+
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
+
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123',
+      };
+
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(200);
+
+      //get accessToken
+      const accessToken = res.body.accessToken;
+      await request(app.getHttpServer())
+        .get('/auth/me')
+        .set('Authorization', `Bearer ${accessToken}+1`)
+        .expect(401);
+    });
+  });
+
+  describe('3. (POST) - LOGOUT USER', () => {
+    it('1. Should logout user and return status code of 204', async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
+
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
+
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123',
+      };
+
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(200);
+
+      // Extract refreshToken from cookie
+      const cookies = res.headers['set-cookie'];
+      const refreshToken = cookies[0].split(';')[0].split('=')[1];
+    
+
+      await request(app.getHttpServer())
+        .post('/auth/logout')
+        .set('Cookie', `refreshToken=${refreshToken}`)
+        .expect(204);
+    });
+
+    it("2. Shouldn't logout user and return status code of 401 if unauthorized", async () => {
+      //create user
+      const newUser = {
+        login: 'Tina',
+        password: 'tina123',
+        email: 'Tina@gmail.com',
+      };
+
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(newUser)
+        .auth('admin', 'qwerty')
+        .expect(201);
+
+      //login user
+      const loginInput = {
+        loginOrEmail: 'Tina',
+        password: 'tina123',
+      };
+
+      const res = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(loginInput)
+        .expect(200);
+
+      // Extract refreshToken from cookie
+      const cookies = res.headers['set-cookie'][0];
+      const refreshToken = cookies[0].split(';')[0].split('=')[1];
+
+      await request(app.getHttpServer())
+        .post('/auth/logout')
+        .set('Cookie', `refreshToken=${refreshToken}+1`)
         .expect(401);
     });
   });
